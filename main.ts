873c1b35531501ca696180955c80ab15e055823c
sophia-s07 <133241675+sophia-s07@users.noreply.github.com>
2023-05-11 11:26:04 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -23,7 +23,7 @@ basic.forever(function () {
     button()
     if (item) {
         basic.showNumber(item)
-        radio.sendNumber(1)
+        radio.sendNumber(item)
     } else if (pins.analogReadPin(AnalogPin.P0) < 400) {
         basic.showString("-X")
     } else if (pins.analogReadPin(AnalogPin.P0) > 600) {
